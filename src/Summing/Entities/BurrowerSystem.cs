e676946e8c988687c40268f8ee4f9ca8f3441901
gongahkia <angryapplegravy@gmail.com>
2026-09-06 22:42:06 +0800

    editedandmadethemansmall
    (cherry picked from commit 1bdf2ca8ebbc4d10d179b7ddaa319904cc60f27e)

---
+++ src/Summing/Entities/BurrowerSystem.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Summing.Gameplay;
+using Summing.Generation;
+using Summing.Player;
+using Summing.Rendering;
+using Summing.World;
+
+namespace Summing.Entities;
+
+public sealed class BurrowerSystem
+{
+    private readonly List<BurrowerEntity> _burrowers = [];
+    private readonly DifficultyTuning _tuning;
+
+    public BurrowerSystem(GeneratedWorld generated, DifficultyTuning tuning)
+    {
+        _tuning = tuning;
+        foreach (var feature in generated.Features)
+            if (feature.Kind == WorldFeatureKind.BurrowerSpawn) Add(feature);
+    }
+
+    public IReadOnlyList<BurrowerEntity> Burrowers => _burrowers;
+    public event Action<string>? Event;
+
+    public void Update(PlayerController player, TileWorld world, float dt)
+    {
+        foreach (var burrower in _burrowers)
+            burrower.Update(player, world, _tuning.EnemySpeedMultiplier, _tuning.EnemyContactDamage, dt);
+    }
+
+    public void ApplyExplosion(Explosion explosion)
+    {
+        foreach (var burrower in _burrowers) burrower.ApplyExplosion(explosion);
+        Event?.Invoke($"explosion:{(int)explosion.Position.X},{(int)explosion.Position.Y}");
+    }
+
+    public void Draw(SpriteBatch batch, SpriteLibrary sprites)
+    {
+        foreach (var burrower in _burrowers)
+            if (burrower.Alive) sprites.DrawBurrower(batch, burrower.Position, burrower.AnimationFrame, burrower.HurtFlash);
+    }
+
+    private void Add(WorldFeature feature)
+    {
+        var burrower = new BurrowerEntity(feature);
+        burrower.DugTile += tile => Event?.Invoke($"dig:{tile.X},{tile.Y}");
+        burrower.Attacked += () => Event?.Invoke("attack");
+        _burrowers.Add(burrower);
+    }
+}
